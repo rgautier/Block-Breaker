dd044f52dce6029e6116495eb533d4b7f338b898
Rich Gautier <rgautier@gmail.com>
2015-03-15 11:45:18 -0400

    Added colliders

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	public LevelManager levelManager;
+
+	void OnCollisionEnter2D(Collision2D collision){
+		Debug.Log ("Ouch! - " + collision.gameObject.name.ToString());
+		levelManager.LoadLevel ("Winner");
+	}
+	void OnTriggerEnter2D(Collider2D colliderObject){
+		Debug.Log ("Triggered! - " + colliderObject.name);
+		levelManager.LoadLevel ("Winner");
+	}
+
+}
