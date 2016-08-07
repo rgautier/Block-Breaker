f65d8f3666c8939bbd1f4dcfbedfb1ae1dbb90fd
Rich Gautier <rgautier@gmail.com>
2016-08-07 09:45:32 -0400

    Upload of Last Edits - unremembered...
    I have no idea what changes are in this commit - they've been sitting
    around forever.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -22,4 +22,10 @@ public class Ball : MonoBehaviour {
 			launched = true;
 		}
 	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		if(collision.gameObject.name == "Paddle"){
+			print("Hit the paddle...");
+		}
+	}
 }
